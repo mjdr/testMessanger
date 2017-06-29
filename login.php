08b71799b485e9952fd3bf9a3a20f76261e62b8d
mjdr <misterjdr@yandex.ru>
2017-06-29 15:46:44 +0300

    Create frontend for login page

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+  <title> Login Page</title>
+ </head>
+ <body>
+  <div>
+   <center>
+    <h2>Login</h2>
+    <form>
+     Login:<br/>
+     <input type="text" name="login"/><br/>
+     Password:<br/>
+     <input type="password" name="passwrd"/><br/>
+     <input type="submit" value="Log in">
+    </form>
+   </center>
+  </div>
+ </body>
+</html>
